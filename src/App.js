d23159b7da6a0070bba71497857f28cb622ef265
bezlin6mechminerz <54895485+bezlin6mechminerz@users.noreply.github.com>
2021-03-09 03:10:06 +0530

    s

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ class App extends Component {
   state = {};
   render() {
     return (
-      <div>
+      <div class="wrapper">
         <h1>Register to start recieving funds.</h1>
         <input type="nmae" id="name" />
+        <br />
         <input type="email" id="email" />
+        <br />
+        <input type="tel" id="tel" />
       </div>
     );
   }
